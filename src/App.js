809045d52957e954a0030e77360ca02cc78cb2fd
Jimmy Li <lijjimmy@gmail.com>
2023-01-26 23:07:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import StoryCard from './components/StoryCard';
+import {Container, Stack, Button} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal">
+        <StoryCard/>
+        <StoryCard/>
+      </Stack>
+      
+
+    </Container>
+
   );
 }
 
---
+++ src/components/StoryCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+
+export default function StoryCard() {
+  return (
+    <Card className="bg-danger">
+      <Card.Body>
+        
+        <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+            This is the card title.
+        </Card.Title>
+        This is the card body.
+      </Card.Body>
+    </Card>
+  )
+}
